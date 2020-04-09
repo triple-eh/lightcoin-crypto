6e428a8341f2060d314a46f113b8e72505855b72
triple-eh <aleckaleksandrov@gmail.com>
2020-04-09 12:22:53 -0700

    added ability to make deposits/withdrawals using a class-based approach

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
+class Transaction {
+  
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
+  }
+  
+  commit() {
+    this.account.balance += this.value;
   }
 
-  commit() {
-    balance -= this.amount;
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
+}
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    // Have the account myAccount.balance start at $0 since that makes more sense.
+    this.balance = 0;
   }
 
 }
 
 
 
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('alex');
+
+let t1 = new Withdrawal(50.25,myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+let t2 = new Withdrawal(9.99,myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Balance:', myAccount.balance);
+
+let t3 = new Deposit(120.00,myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+console.log('Balance:', myAccount.balance);
